263771176b38c24029814ee67f4e7b991f99b192
Chema <mrjark7@gmail.com>
2023-06-13 10:16:17 +0200

    use about useState and Hooks

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,23 +1,39 @@
 // Tarea: Counter App <https://gist.github.com/Klerith/e1a731cc595c00a9794a709062eae757>
 // Conseguida!!
 
-import { Fragment } from "react";
+import { useState } from "react"; // los Hooks en React empiezan con la palabra ' use '
 import './style.css';
 import PropTypes from 'prop-types';
 
 
-const CounterApp = ( { value } ) => {
+// const handleApp = () => { // si no interfiere el value del function component en la function, se coloca fuera para evitar espacio innecesario
+//     // console.log(event);
 
+// }; 
+
+const CounterApp = ( { value } ) => {
+    // const handleApp = (event) => { // si no tiene ninguna interacción con el value o dependencia con la functionComponent, es mejor tenerlo fuera para que no se renderice todas las veces que la mandes a llamar y no ocupe espacio en memoria
+    //     console.log(event);
+    // } 
+    // const [ counter, setCounter ] = useState(0); // useState en este caso manda el valor inicial a la constante, en este caso counter y el segundo argumento que se le manda es para poder modificar el primer elemento, counter, y se sule empezar por la palabra ' set ' y aquí lo vaoms a usar para modificar el counter ya que el cauter en si no se puede modificar por ser una const
+    const [ counter, setCounter ] = useState(value); // si aquí pongo el value, recibe el valor que le has asignado al padre que tenfa por defecto, en este caso el del main.jsx que es 1 
+    const handleApp = () => {
+        // console.log(event);
+        // setCounter( counter + 1); // modificación permitida del counter
+        setCounter( (t) => t +1 ); // otra manera de hacer la suma donde no hace falta saber el nombre de counter, simplemente puedes poner el nombre de lo que quieras en la función
+    }; 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value * 3 } </h2>
-
+            <h2> { counter } </h2> {} 
+            <button onClick={ handleApp /* se podría poner tb s(event) => handleApp(event) */ /*function(event) {console.log();*/ }> {/* creación de un boton con atributos donde pueden ser funciones. Aunque las buenas prácticas dicen que los elementos .js no deben estar dentro del html*/}
+                +1
+            </button>
         </>
     )
 };
 
-CounterApp.propTypes = {
+CounterApp.propTypes = { // la ' p ' minúscula !OJO¡
     value:  PropTypes.number.isRequired,
 };
 
--- 03-counter-app-vite/theory.md
+++ 03-counter-app-vite/theory.md
@@ -1,5 +1,7 @@
 # First APP in React by VITE
 
+[Reactjs.org/documentation](https://es.legacy.reactjs.org/docs/getting-started.html)
+
 Los files de React funcionan con el .jsx donde decimos que vamos a poner código de js y react
 Usaré dependencias de VITE y no de Create React App ya que esta última usa webpack que es algo distinta y máslenta que VITE
 
@@ -9,3 +11,6 @@ Para implementar fragmentos de código de JS en los fragmentos del html en React
 Son las propiedades que se le mandan a los functional component y normalmente se desestructuran. No se mandan en el argumento como **' props '** sino que se usan las **{ }**
 Las props son funcionalidades que fluyen del componente padre, en este caso el main.jsx, al hijo, el FirstApp.jsx
 Las propos son objetos con información
+
+- **Hooks** 
+<https://es.legacy.reactjs.org/docs/hooks-intro.html>
