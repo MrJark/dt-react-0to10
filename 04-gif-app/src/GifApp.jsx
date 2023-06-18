21191453bd5ea549ae3ef7e248542ce778627a5c
Chema <mrjark7@gmail.com>
2023-06-18 17:31:41 +0200

    've added and completed the first task: add new element to category useCase

--- 04-gif-app/src/GifApp.jsx
+++ 04-gif-app/src/GifApp.jsx
@@ -1,13 +1,39 @@
-// import React from 'react';
+// import React from 'react'; // no me hace falta importarlo porque ya está por defecto (solo ytan solo este)
+import { useState } from 'react';
 import './style.css';
 
 export const GifApp = () => {
+
+    const newCategory = 'Pokemon'; // parte de la tarea
+    // Hook: ⬇️ -> estos no pueden ir entre condicionales
+    const [categories, setCategories] = useState([ // useStateSnippet - espacio en momoria para almacenar las categorías
+
+        'Dragon Ball',
+        'One Piece'
+
+    ]);
+    const onAddCategory = (title) => {
+        // Tarea: añadir una nueva categoria al useState (conseguido)
+        setCategories([ // mi forma para la tarea es esta
+            ...categories,
+            newCategory,
+        ]);
+        // otras formas de hacer la tarea
+        // setCategories( cat => [...cat, 'Valorant']);
+    };
+
     return(
         <>
             {/* Titulo */}
             <h1>GifApp</h1>
             {/* Input */}
-
+            <button onClick={onAddCategory}>Add</button>
+            <ul>
+                { categories.map( category => { // .map devuelve un nuevo arreglo haciendo la modifi.que quieras. En este caso, que devuelve las categories
+                    return <li key={category}>{category}</li>
+                })}
+                
+            </ul>
             {/* Lista de Gifs */}
                 {/* Gifs Items */}
         </>
