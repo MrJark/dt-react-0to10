b1753af2bd534730f4debb860d142c7712d2a553
Chema <mrjark7@gmail.com>
2023-06-24 11:19:08 +0200

    task: added functionalities to btns: achieved!

--- 06-hooks-app/src/01-useState/CounterCustomHook.jsx
+++ 06-hooks-app/src/01-useState/CounterCustomHook.jsx
@@ -5,7 +5,7 @@ import { useCounter } from '../hooks/useCounter';
 export const CounterCustomHook = () => {
 
     // La desestructuración dependerá de la manera en que hagas el return del hook. Al ser un objeto, usas las curly brackets, { }, pero si fuera un array, ( ), tendrás que usar los brackets, [ ], para desestructurar
-    const { counter } = useCounter();
+    const { counter, increment, decrement, resets } = useCounter();
 
   return (
     <>
@@ -13,9 +13,21 @@ export const CounterCustomHook = () => {
 
         <hr />
         
-        <button className='btn btn-primary'>+1</button>
-        <button className='btn btn-primary'>Reset</button>
-        <button className='btn btn-primary'>-1</button>
+        <button className='btn btn-primary' onClick=
+          {
+            increment
+          }
+        >+2</button>
+        <button className='btn btn-primary' onClick=
+          {
+            resets
+          }
+        >Reset</button>
+        <button className='btn btn-primary' onClick=
+          {
+            decrement
+          }
+        >-2</button>
 
     </>
   )
--- 06-hooks-app/src/hooks/useCounter.js
+++ 06-hooks-app/src/hooks/useCounter.js
@@ -2,11 +2,31 @@
 
 import { useState } from "react"
 
-export const useCounter = ( initialValue = 1 ) => {
+export const useCounter = ( initialValue = 0 ) => {
 
-    const [counter, setCounter] = useState( initialValue )
+    const [counter, setCounter] = useState( initialValue );
+
+    // Tarea: crear los botones y hacer que funcionen (CONSEGUIDA)
+
+    // para externalizar el setCounter, hay que crear las constantes que van a usar el setCounter:
+    const increment = () => {
+        setCounter(counter + 2);
+    };
+
+    // si fuera una app de compras, no querrias que el carrito bajara de cero, por tanto tendrías que hacer una validación en el decrement como if ( counter === 0 ) return;
+    const decrement = () => {
+        // if (counter === 0) return;
+        setCounter(counter - 2);
+    };
+    
+    const resets = () => {
+        setCounter(initialValue);
+    };
 
     return {
         counter,
+        increment,
+        decrement,
+        resets,
     }
 };
