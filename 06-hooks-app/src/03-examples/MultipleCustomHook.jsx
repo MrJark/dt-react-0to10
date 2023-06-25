732c044d1d959f70b2b51cc3468d8ef31d3092a4
Chema <mrjark7@gmail.com>
2023-06-25 11:27:18 +0200

    doing

--- 06-hooks-app/src/03-examples/MultipleCustomHook.jsx
+++ 06-hooks-app/src/03-examples/MultipleCustomHook.jsx
@@ -5,10 +5,35 @@ export const MultipleCustomHook = () => {
 
     const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character')
 
+    // if( isLoading ){
+    //     return(
+    //         <h1>Cargando...</h1>
+    //     )
+    // }
+
     console.log({ data, isLoading, hasError });
+
     return (
         <>
             <h1>Rinck and Morty Characters</h1>
+            <hr />
+
+            { // usa las validaciones ternarias solo y tan solo si son pocas líneas de código
+                (isLoading) 
+                    ? (
+                        <div className='alert alert-info text-center'>
+                            Loading...
+                        </div>
+                    ) : 
+                    (
+                        <blockquote className='blockquote text-end'>
+                            <p></p>
+                            <footer className='blockquote-footer'>Naval</footer>
+                        </blockquote>
+
+                    )
+            }
+
         </>
 
     )
