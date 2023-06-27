18fc3fe82bc446e6e9fab8fa350877d00559e121
Chema <mrjark7@gmail.com>
2023-06-27 14:56:49 +0200

    using useCallback

--- 06-hooks-app/src/06-memo/CallbackHooks.jsx
+++ 06-hooks-app/src/06-memo/CallbackHooks.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { ShowIncrement } from './ShowIncrement';
 
 
 export const CallbackHooks = () => {
 
-   const [counter, setCounter] = useState(1);
+   const [counter, setCounter] = useState(0);
 
-   const theIncrement = () => {
-    setCounter(counter+1)
-   }
+    const theIncrement = useCallback(
+      () => {
+        // setCounter( counter + 1 );
+        setCounter( (value) => value + 1 );
+      },
+      [],
+    )
+    
+
+//    const theIncrement = () => {
+//     setCounter(counter + 1)
+//    }
 
   return (
     <>
         <h1>useCallBack Hook: { counter } </h1>
         <hr />
 
-        <ShowIncrement increment={theIncrement}/>
+        <ShowIncrement increment={ theIncrement }/>
     </>
   )
 }
--- 06-hooks-app/src/06-memo/ShowIncrement.jsx
+++ 06-hooks-app/src/06-memo/ShowIncrement.jsx
@@ -1,7 +1,11 @@
+import React from "react";
 
 
 // prop que recibo como argumento, increment, es la que necesito en el callback hook ya que de esta dependerá el increment
-export const ShowIncrement = ({increment}) => {
+export const ShowIncrement = React.memo(( { increment } ) => {
+    // NO SOLO puedes usar el React.memo() porque la funnción theIncrement está devolviendo un valor diferente cada vez, por tanto, al ser distinta, no puede guaradar los valores en memoria.
+    // Para evitar que se redibuje se usa el useCallback pero en el componente padre, en este caso el CallbackHooks.jsx
+    console.log('was drawn');
 
   return (
     <button 
@@ -13,4 +17,4 @@ export const ShowIncrement = ({increment}) => {
         Increment
     </button>
   )
-};
+})
