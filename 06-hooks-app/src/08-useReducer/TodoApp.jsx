3679b5563c51c85b5b5d0da1875d7bb59a643b8e
Chema <mrjark7@gmail.com>
2023-06-29 09:55:07 +0200

    added 'save in local Storage' functionality

--- 06-hooks-app/src/08-useReducer/TodoApp.jsx
+++ 06-hooks-app/src/08-useReducer/TodoApp.jsx
@@ -1,25 +1,38 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { TodoAdd, TodoList, todoReducer } from "./index";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        task: 'Obtener Gema del Alma',
-        achieved: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        task: 'Obtener Gema del Poder',
-        achieved: true,
-    },
+    // {
+    //     id: new Date().getTime(),
+    //     task: 'Obtener Gema del Alma',
+    //     achieved: false,
+    // },
+    // {
+    //     id: new Date().getTime() * 3,
+    //     task: 'Obtener Gema del Poder',
+    //     achieved: true,
+    // },
 ];
+
+// es la 3a función del useReducer(puede tener cualquier nombre) y es para inicializar el todos con los que ya teníamos en el localStorage
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || []; // .parse para pasar el string al elemento que era antes, en este caso un objeto. Si no hay nada en el json, que sea un arreglo vacio
+}
 
 
 export const TodoApp = () => {
 
     // solo pasas la rederencia de todoReducer al useReducer para que el mismo la ejecute
-    const [ todos, dispatch ] = useReducer(todoReducer, initialState);
+    const [ todos, dispatch ] = useReducer(todoReducer, initialState, init);
 
+    // Para guardar en Local Storage. Necesitas que cuando los todos (del useReducer) se generen, se guarden el el Local Storage, pro tanto, necesitas a parte de generarlos, que hagan esta otra función
+    useEffect(() => {
+      // en localstorage solo se pueden guardar strings, no objetos y llos todos son objetos. NEcesitas hacer la transformación
+      // localStorage no hay que importarlo ya que viene por defecto en js
+      // para grabarlo en el ls como string. El primero es la key, el segundo, el JSON, el value
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+    
     const handleNewTodo = ( todo ) => {
         const action = {
             type: '[TODO] Add Todo',
