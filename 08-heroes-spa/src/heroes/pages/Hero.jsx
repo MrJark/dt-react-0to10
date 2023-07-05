ad968567ce0c1dc8971b6bb63d45d17687be627c
Chema <mrjark7@gmail.com>
2023-07-05 17:59:41 +0200

    added hero page

--- 08-heroes-spa/src/heroes/pages/Hero.jsx
+++ 08-heroes-spa/src/heroes/pages/Hero.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
 
@@ -7,18 +7,49 @@ export const Hero = () => {
     // const params = useParams(); // hook creado por react-router que sirve para obtener los parámetros
     // console.log(params);
     const { id } = useParams(); // hook creado por react-router que sirve para obtener los parámetros
-    // console.log(id);
-    
-
+    const navegate = useNavigate();
     const hero = getHeroById(id);
 
-    if( !hero ) {
+    // Tarea: hacer que el btn GoBack funcione ( no conseguida, no sabía que tenía que llamar al useNavegate)
+
+    const onGoBack = () => {
+        // return <Navigate to="/marvel"/> // como lo estaba haciendo
+        navegate(-1); // esto lleva al usuario a la pag anterior, incluida sacarlo de la app
+    };
+
+    if( !hero ) { // si no exsite un hero se redirecciona
         return <Navigate to="/marvel"/>
     }
 
     return (
         <>
-            <h1>{hero.superhero}</h1>
+            <div className='row mt-5'>
+                <div className="col-4">
+                    <img 
+                        src={`/assets/heroes/${id}.jpg`}
+                        alt={ hero.superhero }
+                        className='img-thumbnail'    
+                    />
+                </div>
+                <div className="col-8">
+                    <h3>{hero.superhero} </h3>
+                    <ul className='list-group list-group-flush'>
+                        <li className='list-group-item'><b>Alter ego:</b> { hero.alter_ego } </li>
+                        <li className='list-group-item'><b>Publiser: </b> { hero.publisher } </li>
+                        <li className='list-group-item'><b>First Apparence: </b> { hero.first_appearance } </li>
+                        
+                    </ul>
+                    <h5 className='mt-3'>Characters </h5>
+                    <p>{hero.characters}</p>
+                    <hr />
+
+                    <button 
+                        className='btn btn-outline-secondary'
+                        onClick={ onGoBack }
+                    >
+                    Go back</button>
+                </div>
+            </div>
         
         </>
     )
