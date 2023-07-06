ced5f04e3742340e5751ca26f7f3accc4c21ac11
Chema <mrjark7@gmail.com>
2023-07-06 15:37:39 +0200

    the error was that i forgot return heroes.filter(...) on getHeroByName.js :')

--- 08-heroes-spa/src/heroes/helpers/getHeroByName.js
+++ 08-heroes-spa/src/heroes/helpers/getHeroByName.js
@@ -7,7 +7,7 @@ export const getHeroByName = ( name = '' ) => {
 
     if( name.length === 0 ) return [];
 
-    heroes.filter(
+    return heroes.filter(
         hero => hero.superhero.toLocaleLowerCase().includes(name)
     );
 }
