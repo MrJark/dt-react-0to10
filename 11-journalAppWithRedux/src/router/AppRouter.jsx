438aaf2102c6a29ab06685220df369c47b52c75f
Chema <mrjark7@gmail.com>
2023-07-18 16:44:31 +0200

    protected the routes in AppRouter with authenticated

--- 11-journalAppWithRedux/src/router/AppRouter.jsx
+++ 11-journalAppWithRedux/src/router/AppRouter.jsx
@@ -1,13 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes';
 import { JournalRoute } from '../journal/routes';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { CheckingAuth } from '../ui';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../firabase/config';
+import { login, logout } from '../store/auth';
 
 
 export const AppRouter = () => {
 
     const { status } = useSelector( state => state.auth);
+
+    const dispatch = useDispatch(); // hace falta el dispatch para llamar al user
+
+    useEffect( () => { // este useEffect es para disparar 'algo' cuando el usuario esté authenticated o no
+        // Firebase te permite saber que usuario está activo -> onAuthStateChanged() el cual pide tu auth, que en este caso es FirebaseAuth
+        
+        onAuthStateChanged( FirebaseAuth, async (user) => {
+            if (!user ) return dispatch( logout() ); // si no hau user
+
+            const { uid, email, displayName, photoURL } = user;
+            dispatch( login( { uid, email, displayName, photoURL } ) );
+        })
+
+    }, [])
+
+
     if( status === 'checking') {
         return <CheckingAuth/>
     }
@@ -17,9 +37,17 @@ export const AppRouter = () => {
 
         <Routes>
 
-            <Route path='/auth/*' element= { <AuthRoutes/>} />
+            {
+                (status === 'authenticated') // protección de rutas privadas y públicas
+                    ? <Route path='/*' element={ <JournalRoute/> }/>
+                    : <Route path='/auth/*' element= { <AuthRoutes/>} />
+            }
 
-            <Route path='/*' element={ <JournalRoute/> }/>
+            <Route path='/*' element= { <Navigate to='/auth/login'/> }/>
+
+            {/* <Route path='/auth/*' element= { <AuthRoutes/>} /> */}
+
+            {/* <Route path='/*' element={ <JournalRoute/> }/> */}
 
         </Routes>
     )
