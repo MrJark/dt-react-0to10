5b16f2ac760c841ed23a20c789f9d2b2a5da501d
Chema <mrjark7@gmail.com>
2023-07-25 09:28:23 +0200

    solved the error with payload in thunks.test.js. The problem was in the initialization newNote because it was outside the export

--- 11-journalAppWithRedux/__test__/store/journal/thunks.test.js
+++ 11-journalAppWithRedux/__test__/store/journal/thunks.test.js
@@ -1,5 +1,7 @@
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite';
 import { addNewEmptyNote, savingNewNote, setActiveNote } from '../../../src/store/journal';
 import {startNewNote} from '../../../src/store/journal/thunks';
+import { FirebaseDB } from '../../../src/firabase/config';
 
 describe(' test in thunks in journal', () => {
 
@@ -11,15 +13,32 @@ describe(' test in thunks in journal', () => {
     test('should create a new note', async() => {
 
         const uidTest = 'TEST-UID';
-
         getState.mockReturnValue( { auth: { uid: uidTest } } ); // es la creación del auth que está en el thunks con la función getState ( aquí solo estás intentando recrear esa función, no la estás trayendo de ahí que le estés haciendo el mock)
 
         await startNewNote()(dispatch, getState); // estos son los segundos argumentos (los que están en el return) que recibe el startNewNote
-        // esta prueba da error porque a la hora del auth te pide autenticarte y aquí no lo estás haciendo por tanto, el test por 'culpa'de firebase te da error
-        // he tenido que crear una nueva app para poder hacer el checking del usuario con la nueva nota pero queda borrar dicha nota de manera automática al igual que se ha creado
-    
-        espect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
-        
-    });
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+            body: '',
+            title: '',
+            date: expect.any( Number ),
+            id: expect.any( String )
+        }) );
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title: '',
+            date: expect.any( Number ),
+            id: expect.any( String )
+        }) );
+        // este test me estaba dando error porque el payload al inicio me daba undefined y se debía a que tenia el newNote en el thunk fuera del export const  por tanto, siempre iba a dar error, no había nada que renderizar y por tanto, no había ningún title ni datos
 
+        // Boorar datos de firebase para no sobrecargar con las pruebas 
+        // 1º debes apuntar a la collecion que estás creando con la ruta -> ${uid}/journal/notes que es el newDoc
+        const collectionRef = collection( FirebaseDB, `${uidTest}/journal/notes`); // referencia
+        const docs = await getDocs( collectionRef ); // para tener cada una de las entradas
+
+        const deletePromises = [] ; // creas el arreglo donde van a estar todas las promesas a borrar
+        docs.forEach( doc => deletePromises.push( deleteDoc )); // le envias por cada doc, el deleteDoc importado de Firebase para que elimine
+
+        await Promise.all( deletePromises ); // llamas a la función
+    });
 });
--- 11-journalAppWithRedux/src/store/journal/thunks.js
+++ 11-journalAppWithRedux/src/store/journal/thunks.js
@@ -5,28 +5,28 @@ import { addNewEmptyNote, deleteNoteById, savingNewNote, setActiveNote, setNotes
 import { fileUpload, loadNotes } from '../../helpers';
 
 
-const newNote = {
-    title: '',
-    body: '',
-    date: new Date().getTime(),
-};
+// const newNote = {
+//     title: '',
+//     body: '',
+//     date: new Date().getTime(),
+// };
 
 export const startNewNote = () => {
     return async( dispatch, getState ) => { // el return de los thunks tienen dos argumentos, el segundo es una funcion que devuelve el contenido del usuario
         
+        dispatch( savingNewNote( newNote ) );
         const { uid } = getState().auth;// sacas el uid a través del getState ya que te hace falta para la authentication del usuario y recoger las notas qe tenga o en este caso, añadir nuevas
         
         
-        // const newNote = {
-        //     title: '',
-        //     body: '',
-        //     date: new Date().getTime(),
-        // };
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        };
         
-        dispatch( savingNewNote( newNote ) );
         
         const newDock = doc( collection( FirebaseDB, `${uid}/journal/notes` ) ); // la ruta es 'inventada' es decir, puedes poienr lo que quieres pero la más lógica es esta. Que a través de un uid tengas el journal y dentro de este, las notas
-        const setDocRes = await setDoc( newDock, newNote ); // si falla da un error sino, no da nada ( en el log sale undefined )
+        await setDoc( newDock, newNote ); // si falla da un error sino, no da nada ( en el log sale undefined )
         /* con esto solo, te dará error. Lo que debes hacer es lo siguiente:
         1. ir a Firebase
         2. entrar a través del menú a Firestore DB
