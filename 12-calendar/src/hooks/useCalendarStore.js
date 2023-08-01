49752a62eac15563e4096107e2fd8e22a63a6c6d
Chema <mrjark7@gmail.com>
2023-08-01 11:11:06 +0200

    added updater reducer

--- 12-calendar/src/hooks/useCalendarStore.js
+++ 12-calendar/src/hooks/useCalendarStore.js
@@ -1,7 +1,7 @@
 // hook para tener centralizado todos los movimientos/ eventos que se hagan en el calendario
 
 import { useDispatch, useSelector } from "react-redux"
-import { onAddNewEvent, onSetActiveEvent } from "../store/calendar";
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store/calendar";
 
 
 
@@ -19,7 +19,8 @@ export const useCalendarStore = () => {
 
         if( calendarEvent._id ) {
             // siginifa que estoy actualizando porque el _id existe pero estás tacando la nota
-
+            dispatch( onUpdateEvent( calendarEvent ) ); 
+            // dispatch( onUpdateEvent( {...calendarEvent} ) ); // esta es la forma más restrictiva porque con el spread te aseguras de mandar un nuevo objeto con todo igual
         } else {
             // estás creando la nota porque no existe el _id
             dispatch( onAddNewEvent( { ...calendarEvent, _id: new Date().getTime() } ) ) // ( cuando no hay backend) hay que mandarle un id pero como es nuevo, necesitas alho y para eso le mandas el spread y el id como si fuera la date ( mientras no haya backend)
--- 12-calendar/src/store/calendar/calendarSlice.js
+++ 12-calendar/src/store/calendar/calendarSlice.js
@@ -29,9 +29,19 @@ export const calendarSlice = createSlice({
         onAddNewEvent: ( state, { payload } ) => {
             state.events.push( payload ); // para añadir la nota
             state.activeEvent = null; // limpias el evento activo (lo cierra)
-        }
+        },
+        onUpdateEvent: ( state, { payload } ) => {
+            // viene ta con un id proque es un update
+            state.events = state.events.map(event => { // para sobreescribir el evento, ya que el map te crea una copia del objeto que le des con las cambios que le digas, con las modificaciones que hayas hecho
+
+                if ( event._id === payload._id ){
+                    return payload;
+                }
+                return event;
+            });
+        },
     }
 });
 
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } = calendarSlice.actions;
