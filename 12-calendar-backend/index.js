ed78e3f627eece8f3208a49e12c5642a5ae03b0e
Chema <mrjark7@gmail.com>
2023-08-02 11:59:44 +0200

    variable environment

--- 12-calendar-backend/index.js
+++ 12-calendar-backend/index.js
@@ -1,18 +1,25 @@
 const express = require('express'); // muy parecido al import ...
+require('dotenv').config();
+
 
 // Creación del servidor de express
 const app = express();
 
+
+// Directorio Público
+// el .use es solo una función que se ejecuta siempre cada vez que se pasa por una ruta ( es un middelware )
+app.use(express.static('public')); // este es la forma de decirle que habrá la carpeta public cunado quieras ver algo
+
 // Rutas
-app.get('/', (req, res) => {
-    // console.log('se requiere el /');
-    res.json({
-        ok: true
-    })
-});
+// app.get('/', (req, res) => {
+//     // console.log('se requiere el /');
+//     res.json({
+//         ok: true
+//     })
+// });
 
 
 // Para escuchar las peticiones
-app.listen(4000, () => { // el núme del puerto en el que quieres que corra (ojo que no coincida con el del dev) y el segundo arg es un callback
-    console.log(`Server running in port ${4000}`);
+app.listen(process.env.PORT, () => { // el núme del puerto en el que quieres que corra (ojo que no coincida con el del dev) y el segundo arg es un callback
+    console.log(`Server running in port ${process.env.PORT}`); // El .PORT par atener la variable de entorno privada
 }); 
